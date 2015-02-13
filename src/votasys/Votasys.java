c480559eda3259d388b2a2c554a26789aa10f312
jan951002 <jatru95@gmail.com>
2015-02-12 19:09:12 -0500

    Update Votasys.java
    hola mundo

--- src/votasys/Votasys.java
+++ src/votasys/Votasys.java
@@ -16,7 +16,8 @@ public class Votasys {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String saludo;
+        saludo="Hola mundo :D";
     }
     
 }
